b0c0662e49d239fc957249e62cbe8611090c3472
Lukas Wallrich <lukas.wallrich@gmail.com>
2022-09-27 06:00:06 +0000

    version 0.2.0

--- tests/testthat/test-core-and-plot-functions.R
+++ tests/testthat/test-core-and-plot-functions.R
@@ -3,7 +3,18 @@ test_that("GRIM works", {
   expect_true(GRIM_test(5.18, 14, n_items = 2))
   expect_warning(closest <- GRIM_test(5.19, 28, return_values = TRUE))
   expect_equal(closest, 5.18)
-  expect_equal(GRIM_test(5.2, 20, return_values = TRUE), c(5.2, 5.25, 5.15))
+  expect_warning(expect_equal(GRIM_test(5.2, 20, return_values = TRUE), c(5.2, 5.25, 5.15)))
+})
+
+test_that("GRIMMER works", {
+  expect_true(GRIMMER_test(5.21, 1.6, 28))
+  expect_false(GRIMMER_test(3.44, 2.47, 18))
+})
+
+test_that(".sd_limits works", {
+  expect_equal(c(.45, 3.03), .sd_limits(n_obs = 5, mean = 4.2, min_val = 1, max_val = 7, sd_prec = 2))
+  expect_equal(c(.27, 3.03), .sd_limits(n_obs = 5, mean = 4.2, min_val = 1, max_val = 7, sd_prec = 2, n_items = 2))
+  expect_equal(c(0, 0), .sd_limits(n_obs = 100, mean = 1, min_val = 1, max_val = 7))
 })
 
 set.seed(1234)
@@ -16,7 +27,7 @@ test_that("Adjusting mean works", {
 })
 
 restrictions <- list("2.33" = 1, "4" = 3, "4.67" = 0)
-parameters <- set_parameters(3.40, 1.15, 15, 1, 5,
+parameters <- set_parameters(3.29, 1.29, 15, 1, 5,
   m_prec = 2, n_items = 3,
   restrictions_exact = restrictions,
   restrictions_minimum = "range"
